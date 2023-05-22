9ac22a613b170b2946147072c4d51d3a3639864e
Thomas-Marino <thomasmarino627@gmail.com>
2023-05-22 20:03:25 -0300

    creacion del main

---
+++ main.py
@@ -0,0 +1,12 @@
+from funcion_1 import aplicarAumento
+from funcion_2 import reemplazarCaracteres
+from funcion_3 import ordenarCaracteres
+
+precio_para_probar_funcion_1 = 32
+print(aplicarAumento(precio_para_probar_funcion_1))
+
+texto_para_probar_funcion_2 = "holaaaaaaa"
+print(reemplazarCaracteres(texto_para_probar_funcion_2, "a", "reemplazo"))
+
+texto_para_probar_funcion_3 = "algoritmo"
+print(ordenarCaracteres(texto_para_probar_funcion_3, "asc"))
